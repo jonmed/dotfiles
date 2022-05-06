810b8c7abb252afb1d85145d7c548848f9a97c51
Jonatas Medeiros <jonatasmedeiros@servidor.uepb.edu.br>
2022-05-06 13:13:47 -0300

    pomo_check

---
+++ scripts/.local/bin/pomo_check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+PATH=/home/usuario/.local/bin:/usr/bin
+
+TIME=$(pomodoro status --format "%r")
+STATUS_INTERVAL=2
+if [ "$TIME" = "0:00" ]; then
+  pomodoro clear
+  COUNT=10
+  START=$COUNT
+  until [ "$COUNT" -eq "0" ]; do
+    ((COUNT-=1))
+    PERCENT=$((100-100*COUNT/START))
+    echo "#Time remaining$(echo "obase=60;$COUNT" | bc)"
+    echo $PERCENT
+    sleep 1
+  done | zenity --title "Break Timer" --progress --percentage=0 --text ""\
+    --auto-close
+  if zenity --title "Pomodoro" --question  --text "Start new pomodoro?"
+  then
+    pomodoro start
+    TIME=$(pomodoro status --format "%r")
+    notify-send -t 5000 -i info "Time remaining: $TIME"
+  fi
+fi
