ec16a575df63b1f748b20b0214e4852d293ec571
Jonatas Medeiros <jonatasmedeiros@servidor.uepb.edu.br>
2022-11-23 13:20:31 -0300

    Automatic commit: 23-nov-22 13:20:31

--- nvim/.config/nvim/lua/jon/core/colorscheme.lua
+++ nvim/.config/nvim/lua/jon/core/colorscheme.lua
@@ -1,3 +1,12 @@
+local status, kanagawa = pcall(require, 'kanagawa')
+if not status then
+  return
+end
+kanagawa.setup({
+  dimInactive = true,
+  globalStatus = true,
+})
+
 local status, _ = pcall(vim.cmd, 'colorscheme kanagawa')
 if not status then
   print('Colorscheme not found!')
